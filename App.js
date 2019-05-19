731abd9c5681ae741dd4f587d6c1ccbcba2a3a1b
Isaac Bythewood <isaac@bythewood.me>
2019-05-20 06:05:47 +1000

    Add icons to navigation

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { createBottomTabNavigator, createAppContainer, withOrientation } from 'react-navigation';
 import { Text, View, SafeAreaView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from './screens/HomeScreen';
 import EventsScreen from './screens/EventsScreen';
 import AboutScreen from './screens/AboutScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 
 const AppNavigator = createBottomTabNavigator(
@@ -13,7 +15,36 @@ const AppNavigator = createBottomTabNavigator(
     Home: HomeScreen,
     Events: EventsScreen,
     About: AboutScreen,
+    Settings: SettingsScreen,
   },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-home`;
+        }
+        else if (routeName === 'About') {
+          iconName = `ios-information-circle`;
+        }
+        else if (routeName === 'Events') {
+          iconName = `ios-calendar`;
+        }
+        else if (routeName === 'Settings') {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
 );
 
 
