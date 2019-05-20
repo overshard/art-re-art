83b783761b77a55738872b9ae12a46afe2e17380
Isaac Bythewood <isaac@bythewood.me>
2019-05-20 00:15:09 -0400

    Add basic data to about

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 
 import { TitleView } from '../components/Views';
+import AboutData from '../data/about.json';
 
 
 export default class AboutScreen extends React.Component {
@@ -9,6 +10,11 @@ export default class AboutScreen extends React.Component {
     return (
       <ScrollView>
         <TitleView title="About" description="Behind the scenes of Art/Re/Art." />
+        <View style={{ margin: 15 }}>
+          <Text>Website: {AboutData.website}</Text>
+          <Text>Developers: {AboutData.developers}</Text>
+          <Text>Description: {AboutData.description}</Text>
+        </View>
       </ScrollView>
     );
   }
