0af3169c8c101a1904b4e6ad43f5fcb616d275d5
Isaac Bythewood <isaac@bythewood.me>
2019-05-26 16:24:45 -0400

    Accordion component improvements

--- components/Accordion.js
+++ components/Accordion.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default class Accordion extends React.Component {
-  render() {
+  state = {
+    hidden: true,
+  }
+
+  _onPress = () => {
+    this.setState({
+      hidden: !this.state.hidden,
+    })
+  }
+ render() {
     return (
       <View style={{ marginBottom: 30 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 5 }}>
-          {this.props.title}
-        </Text>
-        <Text style={{ fontFamily: "font2", fontSize: 15 }}>
-          {this.props.description}
-        </Text>
+        <TouchableOpacity
+          onPress={this._onPress}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 5 }}>
+            {this.state.hidden ? "+" : "-"} {this.props.title}
+          </Text>
+        </TouchableOpacity>
+        {!this.state.hidden ?
+          <Text style={{ fontFamily: "font2", fontSize: 15 }}>
+            {this.props.description}
+          </Text> : null }
       </View>
     );
   }
