b79ba5de0e21cd801701865929e5040c79d35cf3
Nancy VanNoppen <nancy@vannoppen.co>
2019-05-25 23:03:08 -0400

    add bg to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,8 @@ import {
   View,
   Image,
   ActivityIndicator,
-  Text
+  Text,
+  ImageBackground
 } from "react-native";
 
 import Event from "../components/Event";
@@ -49,53 +50,55 @@ export default class HomeScreen extends React.Component {
     }
 
     return (
-      <ScrollView>
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            margin: 15,
-            marginTop: 30
-          }}
-        >
-          <Image
-            source={require("../assets/images/art-re-art-logo.png")}
+      <ImageBackground source={require("../assets/images/bg4.png")} style={{width: '100%', height: '100%'}}>
+        <ScrollView>
+          <View
             style={{
-              width: Dimensions.get("window").width - 100,
-              height: Dimensions.get("window").width - 100
-            }}
-          />
-        </View>
-        <View
-          style={{
-            margin: 15
-          }}
-        >
-          <Text
-            style={{
-              position: "absolute",
-              color: "white",
-              fontSize: 23,
-              fontWeight: "bold",
-              opacity: 0.3,
-              top: 10,
-              right: 15,
-              zIndex: 2
+              justifyContent: "center",
+              alignItems: "center",
+              margin: 15,
+              marginTop: 30
             }}
           >
-            Next Event
-          </Text>
-          <Event
-            title={this.state.events[0].title}
-            dateDay={this.state.events[0].dateDay}
-            dateMonth={this.state.events[0].dateMonth}
-            dateTime={this.state.events[0].dateTime}
-            locationName={this.state.events[0].location.title}
-            location={this.state.events[0].location.street}
-            event={this.state.events[0]}
-          />
-        </View>
-      </ScrollView>
+            <Image
+              source={require("../assets/images/art-re-art-logo.png")}
+              style={{
+                width: Dimensions.get("window").width - 100,
+                height: Dimensions.get("window").width - 100
+              }}
+            />
+          </View>
+          <View
+            style={{
+              margin: 15
+            }}
+          >
+            <Text
+              style={{
+                position: "absolute",
+                color: "white",
+                fontSize: 23,
+                fontWeight: "bold",
+                opacity: 0.3,
+                top: 10,
+                right: 15,
+                zIndex: 2
+              }}
+            >
+              Next Event
+            </Text>
+            <Event
+              title={this.state.events[0].title}
+              dateDay={this.state.events[0].dateDay}
+              dateMonth={this.state.events[0].dateMonth}
+              dateTime={this.state.events[0].dateTime}
+              locationName={this.state.events[0].location.title}
+              location={this.state.events[0].location.street}
+              event={this.state.events[0]}
+            />
+          </View>
+        </ScrollView>
+      </ImageBackground>
     );
   }
 }
