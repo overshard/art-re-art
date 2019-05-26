54fc01977348ce6f283596433a02863ea453c663
Nancy VanNoppen <nancy@vannoppen.co>
2019-05-25 20:39:26 -0400

    add bg to artists screen

--- screens/ArtistsScreen.js
+++ screens/ArtistsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, FlatList, Text, View } from "react-native";
+import { ScrollView, FlatList, Text, View, ImageBackground } from "react-native";
 
 import { TitleView } from "../components/Views";
 import Artist from "../components/Artist";
@@ -10,26 +10,28 @@ export default class AboutScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView>
-        <TitleView
-          title="Artists"
-          description="All the artists in Art/Re/Art"
-        />
-        <FlatList
-          style={{ margin: 15 }}
-          keyExtractor={this._keyExtractor}
-          renderItem={({ item, index }) => (
-            <Artist
-              index={index}
-              name={item.name}
-              instagram={item.instagram}
-              website={item.website}
-              medium={item.medium}
-            />
-          )}
-          data={ArtistsData.artists}
-        />
-      </ScrollView>
+      <ImageBackground source={require("../assets/images/bg2.png")} style={{width: '100%', height: '100%'}}>
+        <ScrollView>
+          <TitleView
+            title="Artists"
+            description="All the artists in Art/Re/Art"
+          />
+          <FlatList
+            style={{ margin: 15 }}
+            keyExtractor={this._keyExtractor}
+            renderItem={({ item, index }) => (
+              <Artist
+                index={index}
+                name={item.name}
+                instagram={item.instagram}
+                website={item.website}
+                medium={item.medium}
+              />
+            )}
+            data={ArtistsData.artists}
+          />
+        </ScrollView>
+      </ImageBackground>
     );
   }
 }
