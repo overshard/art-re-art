02120fb4e6f1062b72f91481100455444937cb81
Nancy VanNoppen <nancy@vannoppen.co>
2019-05-25 20:13:23 -0400

    style title view

--- components/Views.js
+++ components/Views.js
@@ -6,8 +6,8 @@ import { TitleText } from "./Texts";
 export class TitleView extends React.Component {
   render() {
     return (
-      <View style={{ margin: 15 }}>
-        <TitleText style={{ marginBottom: 5 }}>{this.props.title}</TitleText>
+      <View style={{ marginTop: 30, marginBottom: 30, marginLeft: 15, marginRight: 15, borderBottomColor: "#CCC", borderBottomWidth: 1, paddingBottom: 10, paddingTop: 10 }}>
+        <TitleText style={{ marginBottom: 5, color: "red" }}>{this.props.title}</TitleText>
         {this.props.description ? (
           <Text style={{ color: "#555555", fontFamily: "font2" }}>{this.props.description}</Text>
         ) : null}
