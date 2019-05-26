ac20ae1493b8bfdace482a349a21ff63eadb568a
Isaac Bythewood <isaac@bythewood.me>
2019-05-26 16:07:29 -0400

    Move stack navigator to navigation

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import { Text, View, SafeAreaView, StatusBar } from 'react-native';
-import { AppLoading } from 'expo';
+import { View, StatusBar } from "react-native";
+import { AppLoading } from "expo";
+import { createAppContainer } from "react-navigation";
 
-import cacheAssetsAsync from './utilities/cacheAssetsAsync';
-import AppContainer from './components/AppContainer';
+import cacheAssetsAsync from "./utilities/cacheAssetsAsync";
+import StackNavigator from "./navigation/StackNavigator";
 
+const AppContainer = createAppContainer(StackNavigator);
 
 export default class App extends React.Component {
   state = {
-    appIsReady: false,
+    appIsReady: false
   };
 
   componentWillMount() {
@@ -20,19 +22,19 @@ export default class App extends React.Component {
     try {
       await cacheAssetsAsync({
         images: [
-          require('./assets/images/art-re-art-logo.png'),
-          require('./assets/images/placeholder.jpg'),
+          require("./assets/images/art-re-art-logo.png"),
+          require("./assets/images/placeholder.jpg")
         ],
         fonts: [
           {
-            'font2': require('./assets/fonts/Inconsolata.otf'),
-          },
-        ],
+            font2: require("./assets/fonts/Inconsolata.otf")
+          }
+        ]
       });
     } catch (e) {
       console.warn(
-        'There was an error caching assets (see: main.js), perhaps due to a ' +
-          'network timeout, so we skipped caching. Reload the app to try again.'
+        "There was an error caching assets (see: main.js), perhaps due to a " +
+          "network timeout, so we skipped caching. Reload the app to try again."
       );
       console.log(e.message);
     } finally {
@@ -43,16 +45,13 @@ export default class App extends React.Component {
   render() {
     if (this.state.appIsReady) {
       return (
-        <View style={{ flex: 1, justifyContent: 'center' }}>
-          <StatusBar
-            backgroundColor="black"
-            barStyle="light-content"
-          />
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <StatusBar backgroundColor="black" barStyle="light-content" />
           <AppContainer />
         </View>
       );
     } else {
-      return <AppLoading />
+      return <AppLoading />;
     }
   }
 }
---
+++ navigation/StackNavigator.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import { TouchableOpacity, View, Image } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import { AntDesign } from "@expo/vector-icons";
+
+import ScannerScreen from "../screens/ScannerScreen";
+import FAQScreen from "../screens/FAQScreen";
+
+import BottomTabNavigator from "./BottomTabNavigator";
+
+const StackNavigatorRouteConfigs = {
+  Main: {
+    screen: BottomTabNavigator,
+    navigationOptions: ({ navigation }) => ({
+      headerRight: (
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Scanner")}
+            style={{ marginRight: 20 }}
+          >
+            <AntDesign name="qrcode" size={25} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("FAQ")}
+            style={{ marginRight: 20 }}
+          >
+            <AntDesign name="questioncircleo" size={25} color="white" />
+          </TouchableOpacity>
+        </View>
+      )
+    })
+  },
+  Scanner: {
+    screen: ScannerScreen
+  },
+  FAQ: {
+    screen: FAQScreen
+  }
+};
+
+const StackNavigatorConfig = {
+  headerLayoutPreset: "center",
+  defaultNavigationOptions: {
+    headerTitle: (
+      <Image
+        source={require("../assets/images/art-re-art-logo.png")}
+        style={{
+          width: 100,
+          height: 100,
+          position: "absolute"
+        }}
+      />
+    ),
+    headerStyle: {
+      backgroundColor: "black"
+    },
+    headerBackTitleStyle: {
+      color: "white"
+    },
+    headerBackImage: <AntDesign name="left" size={25} color="white" />
+  }
+};
+
+export default StackNavigator = createStackNavigator(
+  StackNavigatorRouteConfigs,
+  StackNavigatorConfig
+);
