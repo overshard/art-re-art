5db6d3431064f7bc7db993419a8dae76a96ca53f
Nancy VanNoppen <nancy@vannoppen.co>
2019-05-25 20:13:40 -0400

    add questions to FAQ screen

--- screens/FAQScreen.js
+++ screens/FAQScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 
 import { TitleView } from "../components/Views";
 
@@ -11,6 +11,24 @@ export default class FAQScreen extends React.Component {
           title="FAQ"
           description="Frequently Asked Questions"
         />
+        <View style={{ margin: 15 }}>
+          <View style={{ marginBottom: 30 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 5 }}>What is ART/RE/ART?</Text>
+            <Text style={{ fontFamily: "font2" }}>ART/RE/ART is a series of pop-up art shows taking place in and around Morganton, NC.</Text>
+          </View>
+          <View style={{ marginBottom: 30 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 5 }}>Where do the shows take place?</Text>
+            <Text style={{ fontFamily: "font2" }}>ART/RE/ART changes venue with each show. Check out our Events tab to learn more.</Text>
+          </View>
+          <View style={{ marginBottom: 30 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 5 }}>How can I participate in the show?</Text>
+            <Text style={{ fontFamily: "font2" }}>Sign up on our website for email updates and follow us on Instagram to stay tuned about calls for submissions. We also can always use help from volunteers, email us at artreart.morganton@gmail.com if you're interested in volunteering.</Text>
+          </View>
+          <View style={{ marginBottom: 30 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 5 }}>Where can I learn more?</Text>
+            <Text style={{ fontFamily: "font2" }}>Visit our website, www.artreart.com, or email us artreart.morganton@gmail.com.</Text>
+          </View>
+        </View>
       </ScrollView>
     );
   }
