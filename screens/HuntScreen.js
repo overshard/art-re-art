c44a9d7497fb6d647ebbf110c8da14092e045d3e
Isaac Bythewood <isaac@bythewood.me>
2019-05-27 14:44:11 -0400

    Start adding items to the hunt

--- screens/HuntScreen.js
+++ screens/HuntScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View, Text } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 import { TitleView } from "../components/Views";
 
@@ -11,6 +12,57 @@ export default class AboutScreen extends React.Component {
           title="Scavenger Hunt"
           description="Hunts don't unlock till the event starts!"
         />
+        <View
+          style={{
+            flexDirection: "row",
+            margin: 15,
+            padding: 15,
+            backgroundColor: "black",
+            alignItems: "center",
+            borderRadius: 15
+          }}
+        >
+          <View style={{ padding: 15 }}>
+            <AntDesign name="plussquareo" size={25} color="red" />
+          </View>
+          <Text style={{ color: "white", padding: 15 }}>
+            What is the largest piece of art at this event?
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            margin: 15,
+            padding: 15,
+            backgroundColor: "black",
+            alignItems: "center",
+            borderRadius: 15
+          }}
+        >
+          <View style={{ padding: 15 }}>
+            <AntDesign name="checksquare" size={25} color="green" />
+          </View>
+          <Text style={{ color: "white", padding: 15 }}>
+            What is the human first name of who started Art/Re/Art?
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            margin: 15,
+            padding: 15,
+            backgroundColor: "black",
+            alignItems: "center",
+            borderRadius: 15
+          }}
+        >
+          <View style={{ padding: 15 }}>
+            <AntDesign name="plussquareo" size={25} color="red" />
+          </View>
+          <Text style={{ color: "white", padding: 15 }}>
+            What day of the week was Art/Re/Art's first show?
+          </Text>
+        </View>
       </ScrollView>
     );
   }
