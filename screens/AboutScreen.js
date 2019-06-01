274c0b6933c5efa03f33bdca94ab37ed85ac77ae
Jean VanNoppen <jean@vannoppen.co>
2019-06-01 18:25:04 -0400

    connect API to mobile app

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,61 +1,124 @@
 import React from "react";
-import { ScrollView, View, Text, ImageBackground } from "react-native";
+import {
+  ScrollView,
+  FlatList,
+  Text,
+  View,
+  ImageBackground,
+  ActivityIndicator,
+  RefreshControl
+} from "react-native";
 
 import { TitleView } from "../components/Views";
 import Accordion from "../components/Accordion";
 import AboutData from "../data/about.json";
 
 export default class AboutScreen extends React.Component {
+  state = {
+    isLoading: true,
+    about: null,
+  };
+
+  _keyExtractor = (item, index) => item.question;
+
+  _fetchAbout = async () => {
+    this.setState({
+      isLoading: true,
+      about: null,
+    });
+
+    return fetch("https://www.artreart.com/api/about/")
+      .then(res => {
+        return res.json();
+      })
+      .then(resJson => {
+        this.setState({
+          isLoading: false,
+          about: resJson[0]
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  _devs = () => {
+    return this.state.about.developers
+      .map(developers => {
+        return developers.name;
+      })
+      .join(", ");
+  };
+
+  _faqs = () => {
+    return this.state.about.faqs
+      .map(faqs => {
+        return faqs.question + " " + faqs.answer;
+      })
+      .join(", ");
+  };
+
+  componentDidMount() {
+    return this._fetchAbout();
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" />
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
+
     return (
       <ImageBackground
         source={require("../assets/images/bg3.png")}
         style={{ width: "100%", height: "100%" }}
       >
-        <ScrollView>
+        <ScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isLoading}
+              onRefresh={this._fetchAbout}
+            />
+          }
+        >
           <TitleView
-            title="About"
-            description="Behind the scenes of Art/Re/Art."
+            title={this.state.about.title}
+            description={this.state.about.description}
           />
           <View style={{ margin: 15 }}>
-            <View style={{ flexDirection: "row", marginBottom: 10 }}>
+          <View style={{ flexDirection: "row", marginBottom: 10 }}>
               <Text style={{ flex: 1 }}>Website:</Text>
               <Text style={{ flex: 3, fontFamily: "font2" }}>
-                {AboutData.website}
+                {this.state.about.website}
               </Text>
             </View>
             <View style={{ flexDirection: "row", marginBottom: 10 }}>
-              <Text style={{ flex: 1 }}>Dvelopers:</Text>
-              <Text style={{ flex: 3, fontFamily: "font2" }}>
-                {AboutData.developers}
-              </Text>
-            </View>
-            <View style={{ flexDirection: "row", marginBottom: 10 }}>
-              <Text style={{ flex: 1 }}>Description:</Text>
-              <Text style={{ flex: 3, fontFamily: "font2" }}>
-                {AboutData.description}
-              </Text>
+              <Text style={{ flex: 1 }}>Developers:</Text>
+              {this.state.about.developers ? (
+                <Text style={{ flex: 3, fontFamily: "font2" }}>
+                  {this._devs()}
+                </Text>
+              ) : null}
             </View>
           </View>
           <TitleView title="FAQ" description="Frequently Asked Questions" />
-          <View style={{ margin: 15 }}>
+          <FlatList 
+            style={{ margin: 15 }}
+            keyExtractor={this._keyExtractor}
+            renderItem={({ item }) => (
             <Accordion
-              title="What is ART/RE/ART?"
-              description="ART/RE/ART is a series of pop-up art shows taking place in and around Morganton, NC."
+              title={item.question}
+              description={item.answer}
             />
-            <Accordion
-              title="Where do the shows take place?"
-              description="ART/RE/ART changes venue with each show. Check out our Events tab to learn more."
-            />
-            <Accordion
-              title="How can I participate in the show?"
-              description="Sign up on our website for email updates and follow us on Instagram to stay tuned about calls for submissions. We also can always use help from volunteers, email us at artreart.morganton@gmail.com if you're interested in volunteering."
-            />
-            <Accordion
-              title="Where can I learn more?"
-              description="Visit our website, www.artreart.com, or email us artreart.morganton@gmail.com."
-            />
-          </View>
+            )}
+            data={this.state.about.faqs}
+          />
         </ScrollView>
       </ImageBackground>
     );
